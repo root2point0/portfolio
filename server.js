f27c269c31f2d711532214a8878de764e3f466ba
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-08-10 20:17:23 -0700

    preparing to deploy:

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 const express = require('express');
 const app = express();
 
-// const Product = require('../models/product.js');
-// const User = require('../models/user.js');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.urlencoded({extended: true}), express.static(__dirname + '/public'));
